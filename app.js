73ae894de20412d1d950dee92efc12fa26ed56d3
fstipo <fstipanov@gmail.com>
2022-01-06 11:07:46 +0100

    JS start

---
+++ app.js
@@ -0,0 +1,16 @@
+const emailEl = document.querySelector('.email');
+console.log(emailEl);
+const emailLabelEl = document.querySelector('.email-label');
+
+email.addEventListener('focus', (e) => {
+  console.log('Focus');
+  console.log(emailLabelEl);
+  emailLabelEl.style.display = 'none';
+  emailEl.setAttribute('placeholder', 'Please enter your email address');
+  emailEl.style.border = '1px solid #939393';
+});
+
+emailEl.addEventListener('keyup', (e) => {
+  emailLabelEl.style.display = 'none';
+  emailEl.style.border = '3px solid red';
+});
